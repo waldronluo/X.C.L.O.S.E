ffb591016693949539489b7eb79baecf914e1a3e
Luo WeiQiang <154394794@qq.com>
2013-04-15 08:40:06 +0800

    second prototype

--- prototype/home/Home_files/axurerp_pagespecificscript.js
+++
@@ -1,7 +0,0 @@
-﻿for(var i = 0; i < 68; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
-
-$axure.eventManager.pageLoad(
-function (e) {
-
-});
-gv_vAlignTable['u51'] = 'top';gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u55'] = 'center';gv_vAlignTable['u46'] = 'top';gv_vAlignTable['u8'] = 'center';gv_vAlignTable['u60'] = 'center';gv_vAlignTable['u34'] = 'center';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u19'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u41'] = 'top';gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u2'] = 'center';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u52'] = 'top';gv_vAlignTable['u47'] = 'top';gv_vAlignTable['u50'] = 'center';gv_vAlignTable['u28'] = 'center';gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u4'] = 'center';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u65'] = 'top';gv_vAlignTable['u56'] = 'top';gv_vAlignTable['u42'] = 'top';gv_vAlignTable['u63'] = 'top';gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u14'] = 'center';
