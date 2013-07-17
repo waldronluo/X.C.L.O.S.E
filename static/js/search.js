c2696cc2f6b743789b59aea29a761ddffb1f0293
waldron <waldron@5750G>
2013-07-17 14:38:11 +0800

    gaomie

--- static/js/search.js
+++ static/js/search.js
@@ -1,5 +1,8 @@
 (function(){
+    console.log("socket.io");
     var socket = io.connect('http://127.0.0.1:8089');
-    socket.on('',function( ){
+    socket.on('searchPostReply',function(resultArr){
+        console.log("at least");
+        console.log(resultArr);
     });
 })();
