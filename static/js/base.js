8614391e1ee1f4d5fa70e664c352624a7b2f5bbe
waldron <waldron@5750G>
2013-07-21 15:11:22 +0800

    ver0.1.5

--- static/js/base.js
+++ static/js/base.js
@@ -1,11 +1,11 @@
-function signin() {
+function signinWindow() {
     var block = document.getElementById("blockEverything");
     var signin = document.getElementById("signin");
     signin.style.visibility = "visible";
     block.style.visibility = "visible";
 }
 
-function login() {
+function loginWindow() {
     var block = document.getElementById("blockEverything");
     var login = document.getElementById("login");
     login.style.visibility = "visible";
@@ -19,4 +19,20 @@ function X() {
     signin.style.visibility = "hidden";
     login.style.visibility = "hidden";
     block.style.visibility = "hidden";
+}
+
+function login() {
+    console.log("login");
+    var name = document.getElementById("loginUsername");
+    var password = document.getElementById("loginUserPassword");
+    if (GLOBAL.socket != "undefined")
+        GLOBAL.socket.emit ("login", [name.value, password.value]);
+    //console.log ([name.value, password.value]);
+    GLOBAL.socket.on ("loginReply", function (iflogin) {
+        console.log (iflogin);
+        if (iflogin == true) {
+            setCookie ("name", name.value);
+            setCookie ("password", password.value);
+        }
+    });
 }
--- static/js/history.js
+++ static/js/history.js
@@ -1,4 +1,3 @@
-
 var GLOBAL = [];
 GLOBAL.socket = io.connect('http://127.0.0.1:8089');
 
@@ -6,6 +5,46 @@ GLOBAL.socket = io.connect('http://127.0.0.1:8089');
     GLOBAL.socket.on ('historyListReply', function(history){
         console.log(history);
         var table = document.getElementById ("historyTable");
+        table.innerHTML = "";
+        var caption = document.createElement("caption");
+        caption.className = "historyCaption";
+        caption.innerHTML = history[0]['course_title'];
         
+        table.appendChild (caption);
+
+        var th = document.createElement("tr");
+        var ths = [
+        "更新时间","wd200",
+        "版本","wd100",
+        "点赞数","wd100",
+        "修改者","wd100",
+        "修改原因","wd500"
+        ];
+        for ( var i=0 ; i < ths.length ; i += 2) {
+            var head = document.createElement("th");
+            head.className = ths[i+1];
+            head.innerHTML = ths[i];
+            th.appendChild (head);
+        }
+        table.appendChild (th);
+        for ( var i=0 ; i < history.length; i++) {
+            var tr = document.createElement("tr");
+            var updateTime = document.createElement("td");
+            updateTime.innerHTML = history[i]["post_createTime"];
+            var version = document.createElement("td");
+            version.innerHTML = "<a href=/search?post_id="+history[i]["post_id"]+">查看</a>";
+            var likeCounter = document.createElement("td");
+            likeCounter.innerHTML = history[i]["like_count"];
+            var author = document.createElement("td");
+            author.innerHTML = history[i]["post_author"];
+            var reason = document.createElement("td");
+            reason.innerHTML = "";
+            tr.appendChild (updateTime);
+            tr.appendChild (version);
+            tr.appendChild (likeCounter);
+            tr.appendChild (author);
+            tr.appendChild (reason);
+            table.appendChild(tr);
+        }
     });
 })();
